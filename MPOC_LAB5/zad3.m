121daeefb75c765ad1e7826878a312549796fa88
Piotr Zawadzki <115027061+squw0@users.noreply.github.com>
2024-11-23 13:51:17 +0100

    Update zad3.m

--- MPOC_LAB5/zad3.m
+++ MPOC_LAB5/zad3.m
@@ -1,3 +1,4 @@
+pkg load image;
 
 
 function [f_min, f_max] = minMaxTrimmed(I, trim_percent)
@@ -10,29 +11,33 @@ function [f_min, f_max] = minMaxTrimmed(I, trim_percent)
     f_max = 255 - find(cumulative_sum_reverse > trim_pixels, 1, 'first') + 1;
 end
 
+
 function stretched_image = histogramStretchingTrimmed(I, min_val, max_val, trim_percent)
     [f_min, f_max] = minMaxTrimmed(I, trim_percent);
+    disp(['f_min po przycięciu: ', num2str(f_min)]);
+    disp(['f_max po przycięciu: ', num2str(f_max)]);
     I = double(I);
     stretched_image = ((I - f_min) / (f_max - f_min)) * (max_val - min_val) + min_val;
     stretched_image = max(min(stretched_image, max_val), min_val);
     stretched_image = uint8(stretched_image);
 end
 
+
 I = imread('tire.tif');
-figure;
-imshow(I);
-title('Oryginalny obraz');
-figure;
-imhist(I, 256);
-title('Histogram oryginalnego obrazu');
+[f_min, f_max] = minMaxxxxx(I);
+disp(['Minimalna wartość (minMaxxxxx): ', num2str(f_min)]);
+disp(['Maksymalna wartość (minMaxxxxx): ', num2str(f_max)]);
+
 min_val = 0;
 max_val = 255;
 trim_percent = 0.05;
-I_stretch_trimmed = histogramStretchingTrimmed(I, min_val, max_val, trim_percent);
-figure;
-imshow(I_stretch_trimmed);
-title('Obraz z rozciągniętym histogramem (obcięcie 5%)');
-figure;
-imhist(I_stretch_trimmed, 256);
-title('Histogram po rozciągnięciu (obcięcie 5%)');
 
+
+figure;
+subplot(2, 2, 1), imshow(I), title('Oryginalny obraz');
+subplot(2, 2, 2), imhist(I, 256), title('Histogram oryginalnego obrazu');
+
+I_stretch_trimmed = histogramStretchingTrimmed(I, min_val, max_val, trim_percent);
+
+subplot(2, 2, 3), imshow(I_stretch_trimmed), title('Obraz po rozciągnięciu (5% obcięcia)');
+subplot(2, 2, 4), imhist(I_stretch_trimmed, 256), title('Histogram po rozciągnięciu (5% obcięcia)');
