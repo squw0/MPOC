d9b0ca16fbac0121ff578cf1cd1a36d4837f950c
Piotr Zawadzki <115027061+squw0@users.noreply.github.com>
2024-12-10 20:22:37 +0100

    Update zad4.m

--- lab7/zad4.m
+++ lab7/zad4.m
@@ -1,28 +1,26 @@
+%{
 pkg load image;
+clc;
+clear;
 
-img6 = imread('obraz_06a.png');
+I1 = imread('Obraz_06a.jpg');
+I1gray = rgb2gray(I1);
 
-% Poprawa jakości przy użyciu imadjust
-img6_improved = imadjust(img6, [0.2 0.8], [0 1]); % Skalowanie intensywności
-
-
-imwrite(img6_improved, 'obraz_06a_improved.png');
-
-
-
-
-
-%*
-
-pkg load image;
-
-image6 = imread('D:\OM_MPOC\OMishchenko_Lab\Obraz_06a.jpg');
-
-image6_adjusted = imadjust(image6);
+I1adjusted = imadjust(I1gray, stretchlim(I1gray),[0 1]);
 
 figure;
-subplot(1,2,1), imshow(image6), title('Oryginalny obraz 6');
-subplot(1,2,2), imshow(image6_adjusted), title('Poprawiony obraz 6 (imadjust)');
+imshow(I1adjusted);
+title("Obraz 6 imadjust");
 
+%}
 
-%*
+pkg load image;
+
+I1 = imread('Obraz_06a.jpg');
+
+I1adj = imadjust(I1);
+
+figure;
+subplot(1,2,1), imshow(I1), title('Obraz 6');
+subplot(1,2,2), imshow(I1adj), title('Obraz 6 adjusted');
+
