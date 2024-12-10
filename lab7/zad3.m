cc801dfc7afb65511cb87769817fcc2d6786522f
Piotr Zawadzki <115027061+squw0@users.noreply.github.com>
2024-12-10 20:34:07 +0100

    Update zad3.m

--- lab7/zad3.m
+++ lab7/zad3.m
@@ -6,8 +6,13 @@ clear;
 I1 = imread('Obraz_05a.png');
 I1gray = rgb2gray(I1);
 
+brighter = 50;
+I1bright = I1gray + brighter;
+
+
 I1adjusted = histeq(I1gray, 256);
 
 figure;
-subplot(1,2,1), imshow(I1), title("Obraz 5");
-subplot(1,2,2), imshow(I1adjusted), title("Obraz 5 dopasowawny");
+subplot(1,3,1), imshow(I1), title("Obraz 5");
+subplot(1,3,2), imshow(I1bright), title("Obraz 5 jasniejszy");
+subplot(1,3,3), imshow(I1adjusted), title("Obraz 5 znormalizowany");
