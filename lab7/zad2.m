458d2f12e41422610d22857a75e78a0e0e051d10
Piotr Zawadzki <115027061+squw0@users.noreply.github.com>
2024-12-10 20:33:47 +0100

    Update zad2.m

--- lab7/zad2.m
+++ lab7/zad2.m
@@ -9,9 +9,10 @@ I1gray = rgb2gray(I1);
 I1contrast = imadjust(I1gray, stretchlim(I1gray), [0 1]);
 I1h = histeq(I1gray);
 
+I1contrastt = imadjust(I1);
 
 figure;
 subplot(1,3,1), imshow(I1), title("Obraz 4");
-subplot(1,3,2), imshow(I1gray), title("Obraz 4 szary");
+subplot(1,3,2), imshow(I1contrastt), title("Obraz 4 kontrastt");
 subplot(1,3,3), imshow(I1contrast), title("Obraz 4 kontrast");
 
